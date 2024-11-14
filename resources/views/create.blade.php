026926878e9f3c522cfad004d90d65a02fda5c30
akshay-ken <akshaybest3120@gmail.com>
2024-11-14 20:20:50 +0530

    forms and csrf

---
+++ resources/views/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Add Task')
+
+@section('content')
+    <form method="POST" action="{{ route('tasks.store') }}">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input id="title" name="title" text="text" />
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea id="description" name="description" rows="5"></textarea>
+        </div>
+        <div>
+            <label for="long_description">Long Description</label>
+            <textarea id="long_description" name="long_description" rows="10"></textarea>
+        </div>
+
+        <button type="submit">Add Task</button>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,6 +15,8 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
@@ -21,3 +24,7 @@ Route::get('/tasks/{id}', function ($id) {
 Route::fallback(function () {
     return '500E';
 });
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
