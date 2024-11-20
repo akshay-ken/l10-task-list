4f7e8699d827477d6d87cc12cc305791ffa98ff8
akshay-ken <akshaybest3120@gmail.com>
2024-11-20 19:48:40 +0530

    styled forms

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,14 +2,6 @@
 
 @section('title', isset($task) ? 'Edit Task' : 'Add Task')
 
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
 
 @section('content')
     <form method="POST" action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}">
@@ -17,36 +9,40 @@
         @isset($task)
             @method('PUT')
         @endisset
-        <div>
+        <div class="mb-4">
             <label for="title">Title</label>
-            <input id="title" name="title" value="{{ $task->title ?? old('title') }}" text="text" />
+            <input id="title" name="title" value="{{ $task->title ?? old('title') }}" @class(['border-red-500' => $errors->has('title')])
+                text="text" />
             @error('title')
-                <P class="error-message">{{ $message }}</P>
+                <P class="error">{{ $message }}</P>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="description">Description</label>
-            <textarea id="description" name="description" rows="5">
+            <textarea id="description" name="description" rows="5" @class(['border-red-500' => $errors->has('description')])>
                 {{ $task->description ?? old('description') }}</textarea>
             @error('description')
-                <P class="error-message">{{ $message }}</P>
+                <P class="error">{{ $message }}</P>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="long_description">Long Description</label>
-            <textarea id="long_description" name="long_description" rows="10">
+            <textarea id="long_description" name="long_description" rows="10" @class(['border-red-500' => $errors->has('long_description')])>
                 {{ $task->long_description ?? old('long_description') }}</textarea>
             @error('long_description')
-                <P class="error-message">{{ $message }}</P>
+                <P class="error">{{ $message }}</P>
             @enderror
         </div>
-
-        <button type="submit">
-            @isset($task)
-                Update Task
-            @else
-                Add Task
-            @endisset
-        </button>
+        <div class="flex items-center gap-2">
+            <button class="rounded-md px-2 py-1 text-center font-medium shadow-md ring-1 ring-gray-800 hover:bg-blue-400"
+                type="submit">
+                @isset($task)
+                    Update Task
+                @else
+                    Add Task
+                @endisset
+            </button>
+            <a class="link" href="{{ route('tasks.index') }}">Cancel</a>
+        </div>
     </form>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,21 @@
 
     <head>
         <title>Laravel 10 Task list app</title>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-        {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+        {{-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> --}}
+        <script src="https://cdn.tailwindcss.com"></script>
+        <style type="text/tailwindcss">
+            label {
+                @apply block uppercase text-slate-700 mb-2
+            }
+
+            input, textarea {
+                @apply shadow-lg appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none
+            }
+
+            .error {
+                @apply text-red-500 text-sm
+            }
+        </style>
         @yield('styles')
     </head>
 
